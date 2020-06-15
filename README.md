24ec7234e9eed8cd9fa2eced9d1f279a80d0cf20
skbaek <heyori100@gmail.com>
2020-06-15 16:51:46 +0900

    shellscript 수정

--- README.md
+++ README.md
@@ -11,13 +11,15 @@
     3. linux> which java (자바 설치 경로확인 및 복사)
 
 > git source 다운로드
-* git clone https://github.com/100sg/shortening_url.git
+* git clone https://github.com/100sg/shortening_url.git (source file)
+* git clone https://github.com/100sg/h2.git (h2 db file)
     
 > server 구동을 위한 shellscript 실행
+* cd shortening_url (프로젝트 폴더로 이동)
 * shortening_url> ./start.sh {java_path} 로 shellscript를 실행한다. (아래 실행 내용)
     1. h2 DB 구동
     2. 소스 빌드
-    3. project jar 파일 구동 
+    3. project jar file 구동 
  
    
 
--- start.sh
+++ start.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-echo "##### h2 DB Download and Unzip and Run ####"
-cd ..
-mv shortening_url/h2.tar .
-tar -zxvf h2.tar
-.h2/bin/h2.sh
-echo "##### h2 DB success ####"
+echo "##### h2 DB Run start ####"
+./../h2/bin/h2.sh &
+echo "##### h2 DB Run end ####"
 
-echo "##### github source Download and Unzip and Build and Run ####"
-cd shortening_url
+echo "##### source Build and Run ####"
 ./gradlew clean build -x test
 java -jar build/libs/shorteningurl-v0.0.1.jar
 echo "##### github source success ####"
